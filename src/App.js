a76f642f9d8bf301ba6272daedcf18503bc1a168
ibrahim shittu <ibshittu01@gmail.com>
2022-05-04 09:36:37 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useSelector } from 'react-redux';
+import store from './redux/store';
 import './App.css';
 
 function App() {
+
+  const counter = useSelector(state => state.count);
+  const Increment = () => {
+    store.dispatch({ type: 'INCREMENT' });
+  };
+  const Decrement = () => {
+    store.dispatch({ type: 'DECREMENT' });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Counter </h1>
+      <h2>{ counter}</h2>
+      <button onClick={Increment}>
+        Increment
+      </button>
+      
+      <button onClick={Decrement}>
+        Increment
+      </button>
+
     </div>
   );
 }
---
+++ src/redux/store.js
@@ -0,0 +1,23 @@
+import { createStore } from 'redux';
+
+const reducer = (state = { count: 0}, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                ...state,
+                count: state.count + 1
+            };
+        case 'DECREMENT':
+            return {
+                ...state,
+                count: state.count - 1
+            };
+        default:
+            return state;
+    }
+};
+
+
+const store = createStore(reducer);
+
+export default store 
